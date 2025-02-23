8c00f39b99fe0a3a85663bbad9c994a9b8412d99
Retroid <82393276+Bububooo3@users.noreply.github.com>
2025-02-23 17:24:32 -0500

    Updated Constants.lua

---
+++ Constants.lua
@@ -0,0 +1,22 @@
+export type slither_constants = {SpeedRange: NumberRange, Handling: number}
+
+local Constants: slither_constants = {}
+
+Constants.SpeedRange = NumberRange.new(0)
+Constants.Handling = 0
+
+
+
+
+
+
+
+
+
+
+
+--- Returns a table containing immutable game constants.
+return setmetatable({}, {
+	__index = Constants,
+	__newindex = function() error("Attempt to modify read-only table") end
+})
