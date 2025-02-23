434893bf39b7b8973e30685fa0ef14746b1db6e5
Retroid <82393276+Bububooo3@users.noreply.github.com>
2025-02-23 17:24:33 -0500

    Updated Server.lua

---
+++ Server.lua
@@ -0,0 +1,10 @@
+local slither_module = require(script.Main)
+local constants = require(script.Configuration.Constants)
+
+game.Players.PlayerAdded:Connect(function(plr)
+	local moving_bool_val = Instance.new("BoolValue", plr)
+	moving_bool_val.Name = "Moving"
+	moving_bool_val.Value = false
+	
+	slither_module.new(plr)
+end)
